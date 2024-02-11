0be640d0b511db84708247329bd9fbb648c2c210
Kirillov1515 <kirillov_sasha98@mail.ru>
2024-02-11 13:20:18 +0300

    Add TabBarController and NavigationController

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Macbook on 11.02.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+}
